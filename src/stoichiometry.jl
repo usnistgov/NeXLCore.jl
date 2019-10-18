188e62be260244173a4ae7cce531c939a2f71f92
Nicholas W.M. Ritchie <nicholas.wm.ritchie@gmail.com>
2019-10-17 20:57:01 -0400

    Hide the implementation.

--- src/stoichiometry.jl
+++ src/stoichiometry.jl
@@ -4,19 +4,6 @@ const valence = (  1, 0, 1, 2, 3, 4, 5, -2, 1, 0, 1, 2, 3, 4, 5, 6, 5, 0, 1, 2, 
                    3, 3, 3, 3, 3, 4, 5, 6, 4, 4, 4, 4, 3, 2, 1, 2, 3, 4, 5, 0, 1, 2,
                    3, 4, 5, 4, 4, 4 )
 
-function buildoxidefraction(elm::Element, val=valence)
-    den = gcd(val[z(elm)], -val[z(n"O")])
-    Dict{Element, Int}( n"O"=>Base.convert(Int, val[z(elm)] // den),
-                         elm=> Base.convert(Int, -val[z(n"O")]//den))
-end
-
-function buildoxidename(elm::Element, val=valence)::String
-    nn(n) = n>1 ? "$(n)" : ""
-    den = gcd(val[z(elm)], -val[z(n"O")])
-    ne = Base.convert(Int,-val[z(n"O")]//den)
-    no = Base.convert(Int,val[z(elm)] //den)
-    return "$(symbol(elm))$(nn(ne))O$(nn(no))"
-end
 
 """
     asoxide(elm::Element, val = valence)
@@ -24,8 +11,21 @@ end
 Compute the oxidized form of the specified element using the valences provided in <code>val</code>.  By default,
 <code>val = NeXLCore.valences</code>, a typical set of valences.
 """
-asoxide(elm::Element, val = valence) =
+function asoxide(elm::Element, val = valence)
+    function buildoxidefraction(elm::Element, val=valence)
+        den = gcd(val[z(elm)], -val[z(n"O")])
+        Dict{Element, Int}( n"O"=>Base.convert(Int, val[z(elm)] // den),
+                             elm=> Base.convert(Int, -val[z(n"O")]//den))
+    end
+    function buildoxidename(elm::Element, val=valence)::String
+        nn(n) = n>1 ? "$(n)" : ""
+        den = gcd(val[z(elm)], -val[z(n"O")])
+        ne = Base.convert(Int,-val[z(n"O")]//den)
+        no = Base.convert(Int,val[z(elm)] //den)
+        return "$(symbol(elm))$(nn(ne))O$(nn(no))"
+    end
     atomicfraction(buildoxidename(elm, val), buildoxidefraction(elm, val))
+end
 
 """
     asoxide(elm::Element, val = valence)

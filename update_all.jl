dc9d1809da13e9c59783ee67d76f514f8043f9bc
nritchie <nicholas.ritchie@nist.gov>
2019-12-18 11:03:47 -0500

    Restoring Manifest.toml

---
+++ update_all.jl
@@ -0,0 +1,25 @@
+;cd
+;cd .julia/dev/FFAST
+]activate .
+]up
+;cd ../BoteSalvatICX
+]activate .
+]up
+;cd ../NeXLUncertainties
+]activate .
+]up
+;cd ../NeXLCore
+]activate .
+]up
+;cd ../NeXLSpectrum
+]activate .
+]up
+;cd ../NeXLMatrixCorrection
+]activate .
+]up
+;cd ../NeXLMatrixParticle
+]activate .
+]up
+;cd ../NeXLDatabase
+]activate .
+]up

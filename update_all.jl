742eb51bffe9027760472db9e527cc5350215ec9
Nicholas W.M. Ritchie <nicholas.wm.ritchie@gmail.com>
2020-01-19 12:45:07 -0500

    refactored update_all.jl

--- update_all.jl
+++ update_all.jl
@@ -1,27 +1,21 @@
+using Pkg
 # Base system
 Pkg.activate()
 Pkg.gc()
+Pkg.add(PackageSpec(url="https://github.com/usnistgov/BoteSalvatICX.jl.git"))
+Pkg.add(PackageSpec(url="https://github.com/usnistgov/FFAST.jl.git"))
+Pkg.add(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLUncertainties.jl.git"))
+Pkg.add(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLCore.jl.git"))
+Pkg.add(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLSpectrum.jl.git"))
+Pkg.add(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLMatrixCorrection.jl.git"))
+Pkg.add(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLParticle.jl.git"))
+Pkg.instantiate()
 Pkg.update()
+Pkg.develop(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLUncertainties.jl.git"))
+Pkg.develop(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLCore.jl.git"))
+Pkg.develop(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLSpectrum.jl.git"))
+Pkg.develop(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLMatrixCorrection.jl.git"))
+Pkg.develop(PackageSpec(url="https://github.com/NicholasWMRitchie/NeXLParticle.jl.git"))
+#Pkg.build()
 Pkg.precompile()
-# NeXLUncertainties
-Pkg.activate(joinpath(homedir(),".julia/dev/NeXLUncertainties"))
-Pkg.update()
-Pkg.precompile()
-# NeXLCore
-Pkg.activate(joinpath(homedir(),".julia/dev/NeXLCore"))
-Pkg.update()
-Pkg.precompile()
-# NeXLMatrixCorrection
-Pkg.activate(joinpath(homedir(),".julia/dev/NeXLMatrixCorrection"))
-Pkg.update()
-Pkg.precompile()
-# NeXLSpectrum
-Pkg.activate(joinpath(homedir(),".julia/dev/NeXLSpectrum"))
-Pkg.update()
-Pkg.precompile()
-# NeXLParticle
-Pkg.activate(joinpath(homedir(),".julia/dev/NeXLParticle"))
-Pkg.update()
-Pkg.precompile()
-
 Pkg.activate()

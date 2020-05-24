4393b896737bd6350e1247c99023aae00c1bf273
Nicholas Ritchie <NicholasWMRitchie@users.noreply.github.com>
2020-05-24 16:41:11 -0400

    Add more eta tests.

--- test/eta.jl
+++ test/eta.jl
@@ -7,10 +7,11 @@ using Test
     @test isapprox(η(LoveScott1978η, n"Ce", 10.0e3),0.44044972,atol=1.0e-5)
     @test isapprox(η(LoveScott1978η, n"U", 12.0e3),0.5104889,atol=1.0e-5)
 
-    @test isapprox(η(Tomlin1963, n"C", 10.0e3),0.08432227,atol=1.0e-5)
-    @test isapprox(η(Tomlin1963, n"Fe", 30.0e3),0.2804824,atol=1.0e-5)
-    @test isapprox(η(Tomlin1963, n"Ce", 10.0e3),0.44044972,atol=1.0e-5)
-    @test isapprox(η(Tomlin1963, n"U", 12.0e3),0.5104889,atol=1.0e-5)
+    @test η(Tomlin1963, n"H", 10.0e3) > 0.0
+    @test isapprox(η(Tomlin1963, n"C", 10.0e3),0.04862657,atol=1.0e-5)
+    @test isapprox(η(Tomlin1963, n"Fe", 30.0e3),0.2930160,atol=1.0e-5)
+    @test isapprox(η(Tomlin1963, n"Ce", 10.0e3),0.42674050,atol=1.0e-5)
+    @test isapprox(η(Tomlin1963, n"U", 12.0e3),0.5036314,atol=1.0e-5)
 
-
+    @test all(map(z->η(LoveScott1978η, elements[z], 15.0e3)==η(elements[z], 15.0e3),1:99))
 end

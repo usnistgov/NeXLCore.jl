6ec85821bd9dceabc5b839a6e7f9efabeebcfff2
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2020-06-17 12:27:22 -0400

    Oops!

--- src/bethe.jl
+++ src/bethe.jl
@@ -7,18 +7,23 @@ struct Bethe <: BetheEnergyLoss end
 struct JoyLuo <: BetheEnergyLoss end
 
 dEds(::Type{Bethe}, e::Float64, elm::Element, ρ::Float64; mip::Type{<:NeXLMeanIonizationPotential}=Berger1982) =
-     (-785.0e8*ρ*z(elm))/(a(elm)*e)*log(1.116e/J(mip, elm))
+     (-785.0e8*ρ*z(elm))/(a(elm)*e)*log(1.166e/J(mip, elm))
 
+"""
+    dEds(::Type{<:BetheEnergyLoss}, e::Float64, elm::Element, ρ::Float64; mip::Type{<:NeXLMeanIonizationPotential}=Berger1982)
+    dEds(::Type{<:BetheEnergyLoss}, e::Float64, mat::Material, inclDensity=true; mip::Type{<:NeXLMeanIonizationPotential}=Berger1982)
 
+Calculate the loss per unit path length for an electron in the specified element and density.  The results in energy
+loss in eV/Å.
+"""
 function dEds(::Type{JoyLuo}, e::Float64, elm::Element, ρ::Float64; mip::Type{<:NeXLMeanIonizationPotential}=Berger1982)
     k, j = 0.731 + 0.0688 * log(10.0,z(elm)), J(mip, z(elm))
-    return (-785.0e8*ρ*z(elm))/(a(elm)*e)*log(1.116*(e+k*j)/j)
+    jp = j / (1.0 + k*j/e)
+    return ((-785.0e8*ρ*z(elm))/(a(elm)*e))*log(1.166*e/jp)
 end
-
 
 dEds(ty::Type{<:BetheEnergyLoss}, e::Float64, mat::Material; mip::Type{<:NeXLMeanIonizationPotential}=Berger1982) =
     mapreduce(elm->dEds(ty, e, elm, density(mat), mip=mip)*mat[elm], +, keys(mat))
-
 
 Base.range(ty::Type{<:BetheEnergyLoss}, mat::Material, e0::Float64, inclDensity=true; emin=50.0, mip::Type{<:NeXLMeanIonizationPotential}=Berger1982) =
     quadgk(e->1.0/dEds(ty, e, mat, mip=mip), e0, emin, rtol=1.0e-6)[1]*(inclDensity ? 1.0 : density(mat))

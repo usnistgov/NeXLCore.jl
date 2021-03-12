1f1f79182c86d662205c08c9e67dbfca075d054b
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2021-03-12 11:05:08 -0500

    Changed asoxide() for elm=>qty pairs to return a Dict{Material,AbstractFloat} equivalent as oxides.

--- src/stoichiometry.jl
+++ src/stoichiometry.jl
@@ -105,7 +105,7 @@ Compute the oxidized form of the specified element using the valences provided i
 function asoxide(
     elm::Element;
     valences = valences,
-    name = missing,
+    name = nothing,
     atomicweights::Dict{Element,<:AbstractFloat} = Dict{Element,Float64}(),
 )
     function buildoxidefraction(elm, val)
@@ -113,12 +113,13 @@ function asoxide(
         return (n"O" => val[z(elm)] ÷ den, elm => -val[z(n"O")] ÷ den)
     end
     function buildoxidename(elm, val)::String
-        nn(n) = n > 1 ? "$(n)" : ""
+        nnn(n) = n >=10 ? nnn(n÷10)*nnn(n%10) : ("₀","₁","₂","₃","₄","₅","₆","₇","₈","₉" )[n%10+1] 
+        nn(n) = n==1 ? "" : nnn(n)
         den = gcd(val[z(elm)], -val[z(n"O")])
         ne, no = -val[z(n"O")] ÷ den, val[z(elm)] ÷ den
         return "$(symbol(elm))$(nn(ne))O$(nn(no))"
     end
-    name = ismissing(name) ? buildoxidename(elm, valences) : name
+    name = something(name, buildoxidename(elm, valences))
     return atomicfraction(
         name,
         buildoxidefraction(elm, valences)...,
@@ -127,47 +128,74 @@ function asoxide(
 end
 
 """
-    asoxide(elms::Pair{Element, <:AbstractFloat}...; valences = valences, name::Union{AbstractString,Nothing}=nothing)
-    asoxide(elms::Dict{Element, <:AbstractFloat}...; valences = valences, name::Union{AbstractString,Nothing}=nothing)
+    asoxide(elms::Pair{Element, <:AbstractFloat}...; valences = valences, atomicweights::Dict{Element,<:AbstractFloat} = Dict{Element,Float64}())
+    asoxide(elms::Dict{Element, <:AbstractFloat}...; valences = valences, atomicweights::Dict{Element,<:AbstractFloat} = Dict{Element,Float64}())
 
-Compute a mixture of the oxidized forms of the specified elements using the valences provided in `val`.
-By default, `val = NeXLCore.valences`, a typical set of valences.
+Providing the mass-fraction of the consituent elements in `elms`, compute the corresponding amounts of the oxide forms
+of the elements.  This can be used to answer the question: If I measure this amount of these elements, what mass fraction of
+the oxide-forms of each element does this correspond to?  The example below demonstrates that Albite is 68.74% SiO₂ by mass.
+By default, `val = NeXLCore.valences`, a typical set of valences.  See also `obystoichiometry(...)`
+
+Example:
+
+    julia> mat"NaAlSi3O8"
+      NaAlSi3O8[Al=0.1029,Na=0.0877,Si=0.3213,O=0.4881]
+    julia> asoxide(n"Al"=>0.1029, n"Na"=>0.0877, n"Si"=>0.3213)
+      Dict{Material, Float64} with 3 entries:
+      SiO₂[O=0.5326,Si=0.4674]  => 0.687366
+      Al₂O₃[Al=0.5293,O=0.4707] => 0.194424
+      Na₂O[Na=0.7419,O=0.2581]  => 0.118216
+    julia> sum(asoxide(n"Al"=>0.1029, n"Na"=>0.0877, n"Si"=>0.3213), name="Albite")
+      Albite[Al=0.1029,Na=0.0877,O=0.4881,Si=0.3213]
+    julia> asoxide(filter(kv->kv[1]!=n"O", massfraction(mat"NaAlSi3O8")))
+      Dict{Material, Float64} with 3 entries:
+        SiO₂[O=0.5326,Si=0.4674]  => 0.687401
+        Al₂O₃[Al=0.5293,O=0.4707] => 0.194418
+        Na₂O[Na=0.7419,O=0.2581]  => 0.118181
+
 """
-function asoxide(
-    elms::Dict{Element,<:AbstractFloat};
+function asoxide( 
+    elms::Pair{Element,<:AbstractFloat}...; 
     valences = valences,
-    name::Union{AbstractString,Nothing} = nothing,
-    atomicweights::Dict{Element,<:AbstractFloat} = Dict{Element,Float64}(),
-)
-    bname =
-        isnothing(name) ?
-        join(
-            (repr(qty) * asoxide(elm, valences = valences).name for (elm, qty) in elms),
-            "+",
-        ) : name
-    mats = (
-        massfraction(
-            qty * asoxide(elm, valences = valences, atomicweights = atomicweights),
-        ) for (elm, qty) in elms
+    atomicweights::Dict{Element,<:AbstractFloat} = Dict{Element,Float64}()
+) :: Dict{Material, AbstractFloat}
+    return  Dict{Material, AbstractFloat}(
+        map(elms) do (elm, f)
+            @assert elm ≠ n"O" "Don't include the element O in the `elms` argument to asoxide(...)."
+            ox = asoxide(elm, valences=valences, atomicweights=atomicweights)
+            ox=>(f/ox[elm])
+        end
     )
-    return material(bname, merge(+, mats...))
 end
-asoxide(
-    elms::Pair{Element,<:AbstractFloat}...;
+function asoxide( 
+    elms::Dict{Element, <:AbstractFloat};
     valences = valences,
-    name::Union{AbstractString,Nothing} = nothing,
-    atomicweights::Dict{Element,<:AbstractFloat} = Dict{Element,Float64}(),
-) = asoxide(Dict(elms), valences = valences, name = name, atomicweights = atomicweights)
+    name = nothing, 
+    atomicweights::Dict{Element,<:AbstractFloat} = Dict{Element,Float64}()
+)
+    return  Dict{Material, AbstractFloat}(
+        map(collect(keys(elms))) do elm
+            @assert elm ≠ n"O" "Don't include the element O in the `elms` argument to asoxide(...)."
+            ox = asoxide(elm, valences=valences, atomicweights=atomicweights)
+            ox=>(elms[elm]/ox[elm])
+        end
+    )
+end
 
 """
     obystoichiometry(elms::Pair{Element, <:AbstractFloat}..., valences = valences)
     obystoichiometry(elms::Dict{Element, <:AbstractFloat}; valences = valences)
 
 Compute O-by-stoichiometry from the provided mass fractions of elements.
+
+Example:
+
+    obystoichiometry(n"Mg"=>0.1099, n"Al"=>0.0443, n"Si"=>0.1941, n"Ca"=>0.1034, n"Fe"=>0.0756)
+    0.39582340257233467
 """
 obystoichiometry(elms::Pair{Element,<:AbstractFloat}...; valences = valences) = sum(
     f * (-valences[z(elm)] * a(n"O")) / (a(elm) * valences[z(n"O")]) for (elm, f) in elms
 )
 obystoichiometry(elms::Dict{Element,<:AbstractFloat}; valences = valences) = sum(
     f * (-valences[z(elm)] * a(n"O")) / (a(elm) * valences[z(n"O")]) for (elm, f) in elms
-) 
+)
--- test/stoichiometry.jl
+++ test/stoichiometry.jl
@@ -18,16 +18,38 @@ using NeXLCore
     )
 
     sio2 = asoxide(n"Si")
-    @test name(sio2) == "SiO2"
+    @test name(sio2) == "SiO₂"
     asio2 = atomicfraction("SiO2", n"Si" => 1, n"O" => 2)
+    asio2 = atomicfraction("SiO₂", n"Si" => 1, n"O" => 2)
     @test isapprox(sio2[n"Si"], asio2[n"Si"], atol = 1.0e-6)
     @test isapprox(sio2[n"O"], asio2[n"O"], atol = 1.0e-6)
 
-    mix = asoxide(n"Si" => 0.4, n"Al" => 0.6)
-    # Check against DTSA-II
-    @test isapprox(mix[n"O"], 0.4955, atol = 0.0001)
-    @test isapprox(mix[n"Al"], 0.3176, atol = 0.0001)
-    @test isapprox(mix[n"Si"], 0.1870, atol = 0.0001)
+    mix = sum(asoxide(           
+        n"Mg" => 0.116567,
+        n"Al" => 0.0490615,
+        n"Si" => 0.211982,
+        n"Ca" => 0.10899,
+        n"Fe" => 0.0774196))
+    
+    @test isapprox(mix[n"O"], 0.4276, atol = 0.0001)
+    @test isapprox(mix[n"Al"], 0.0490615, atol = 0.0001)
+    @test isapprox(mix[n"Fe"], 0.0774196, atol = 0.0001)
+
+
+    ox = sum(asoxide(filter(elm->elm[1]!=n"O", massfraction(srm470_k412))), name="K412")
+    @test all(isapprox(ox[elm], srm470_k412[elm], atol=1.0e-8) for elm in keys(srm470_k412))
+
+    ox = sum(asoxide(filter(elm->elm[1]!=n"O", massfraction(srm470_k411))), name="K411")
+    @test all(isapprox(ox[elm], srm470_k411[elm], atol=1.0e-8) for elm in keys(srm470_k411))
+
+    m=sum(asoxide(n"Al"=>0.1029, n"Na"=>0.0877, n"Si"=>0.3213), name="Albite", density=5.0, description="a common mineral", pedigree="Natural")
+    @test isapprox(m[n"O"], 0.4881, atol=0.0001)
+    @test isapprox(m[n"Si"], 0.3212, atol=0.0001)
+    @test isapprox(m[n"Na"], 0.0877, atol=0.0001)
+    @test m[:Density] == 5.0
+    @test m[:Description] == "a common mineral"
+    @test m[:Pedigree] == "Natural"
+    @test name(m) == "Albite"
 
     amix =
         0.4 * atomicfraction("SiO2", n"Si" => 1, n"O" => 2) +

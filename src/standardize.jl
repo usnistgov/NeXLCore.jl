e9b5ec888564f9a2a36ab7e130abecb05b7784d6
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2021-04-22 21:01:18 -0400

    Fixing issues with standardization using KRatio structs

--- src/standardize.jl
+++ src/standardize.jl
@@ -1,24 +1,21 @@
-
 """
     suitable_as_standard(kr::KRatio)::Boolean
 
 Does this k-ratio have all the necessary basic properties required for use as a standard 
 (:Composition, :TakeOffAngle, and :BeamEnergy for both `stdProps` and `unkProps`.)
 """
-function suitable_as_standard(std::KRatio)::Boolean
+function suitable_as_standard(std::KRatio)::Bool
     b1=haskey(std.unkProps, :Composition) 
-    b1 || @warn "The :Composition property needs to be specified for the `unknown` in $kr."
+    b1 || @warn "The :Composition property needs to be specified for the `unknown` in $std."
     b2 = haskey(std.unkProps, :BeamEnergy) 
-    b2 || @warn "The :BeamEnergy property needs to be specified for the `unknown` in $kr."
+    b2 || @warn "The :BeamEnergy property needs to be specified for the `unknown` in $std."
     b3 = haskey(std.unkProps, :TakeOffAngle) 
-    b3 || @warn "The :TakeOffAngle property needs to be specified for the `unknown` in $kr."
-    b4 = haskey(std.stdProps, :Composition) 
-    b4 || @warn "The :Composition property needs to be specified for the `standard` in $kr."
-    b5 = haskey(std.stdProps, :BeamEnergy) 
-    b5 || @warn "The :BeamEnergy property needs to be specified for the `standard` in $kr."
-    b6 = haskey(std.stdProps, :TakeOffAngle) 
-    b6 || @warn "The :TakeOffAngle property needs to be specified for the `standard` in $kr."
-    return b1 && b2 && b3 && b4 && b5 && b6
+    b3 || @warn "The :TakeOffAngle property needs to be specified for the `unknown` in $std."
+    b4 = haskey(std.stdProps, :BeamEnergy) 
+    b4 || @warn "The :BeamEnergy property needs to be specified for the `standard` in $std."
+    b5 = haskey(std.stdProps, :TakeOffAngle) 
+    b5 || @warn "The :TakeOffAngle property needs to be specified for the `standard` in $std."
+    return b1 && b2 && b3 && b4 && b5
 end
 
 
@@ -31,12 +28,9 @@ stdProps[:Composition] items match if 1) are Materials with almost the same mass
 AbstractString values.  The later is for 
 """
 function matches(unk::Union{KRatio, KRatios}, std::KRatio)::Bool
-    equiv(c1, c2) = false
-    equiv(c1::AbstractString, c2::AbstractString) = isequal(c1, c2) 
-    equiv(c1::Material, c2::Material) = isapprox(c1, c2, atol=1.0e-5) 
     up, sp = unk.stdProps, std.stdProps
     return element(unk)==element(std) && #  Same element 
-        equiv(up[:Composition], sp[:Composition]) && # Relative to same material
+        isapprox(unk.standard, std.standard, atol=1.0e-5) && # Relative to same material
         issetequal(unk.lines, std.lines) && # Same lines 
         isapprox(sp[:BeamEnergy], up[:BeamEnergy], rtol=0.001) && # Same E0
         isapprox(sp[:TakeOffAngle], up[:TakeOffAngle], atol=deg2rad(0.1)) # Same take-off angle
@@ -56,7 +50,7 @@ function standardize(kr::KRatio, std::KRatio)::KRatio
         s = sqrt((σ(n)/value(d))^2+((value(n)*σ(d))/(value(d)*value(d)))^2)
         return s > 0 ? UncertainValue(f, s) : f
     end
-    return matches(kr, std) ? KRatio(kr.lines, kr.unkProps, std.kratio.unkProps, std.material, div(kr.kratio, std.kratio)) : kr
+    return matches(kr, std) ? KRatio(kr.lines, kr.unkProps, std.unkProps, std.unkProps[:Composition], div(kr.kratio, std.kratio)) : kr
 end
 function standardize(krs::KRatios, std::KRatio)::KRatios
     function div(n, d)
@@ -64,7 +58,7 @@ function standardize(krs::KRatios, std::KRatio)::KRatios
         s = sqrt((σ(n)/value(d))^2+((value(n)*σ(d))/(value(d)*value(d)))^2)
         return s > 0 ? UncertainValue(f, s) : f
     end
-    return matches(krs, std) ? KRatios(krs.lines, krs.unkProps, std.kratio.unkProps, std.material, map(kr->div(kr, std.kratio), krs.kratios)) : krs 
+    return matches(krs, std) ? KRatios(krs.lines, krs.unkProps, std.unkProps, std.unkProps[:Composition], map(kr->div(kr, std.kratio), krs.kratios)) : krs 
 end
 
 function standardize(kratios::Union{AbstractVector{KRatio},AbstractVector{KRatios}}, stds::AbstractVector{KRatio})
--- test/standard.jl
+++ test/standard.jl
@@ -4,63 +4,65 @@ using Test
     @testset "Standardize KRatio" begin
         fe1 = KRatio(
             characteristic(n"Fe", kalpha),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             uv(0.6517, 0.02),
         ) 
+        @test suitable_as_standard(fe1)
         fe2 = KRatio(
             characteristic(n"Fe", ltransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             uv(0.3217, 0.02),
         )
+        @test suitable_as_standard(fe2)
         ca1 = KRatio(
             characteristic(n"Ca", kalpha),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Ca5(PO4)3F"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"CaF2",
             uv(0.7280, 0.02),
         )
+        @test suitable_as_standard(ca1)
         ca2 = KRatio(
             characteristic(n"Ca", kbeta),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Ca5(PO4)3F"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"CaF2",
             uv(0.7326, 0.02),
         )
+        @test suitable_as_standard(ca2)
         si1 = KRatio(
             characteristic(n"Si", ktransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"SiO2"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Si",
             uv(0.3801, 0.02),
         )
-        fe1std, fe2std = Standard(mat"Fe2O3", fe1), Standard(mat"Fe2O3", fe2)
-        ca1std, ca2std = Standard(mat"Ca5(PO4)3F", ca1), Standard(mat"Ca5(PO4)3F", ca2)
-        sistd = Standard(mat"SiO2", si1)
-        @test matches(KRatio(
+        @test suitable_as_standard(si1)
+        @test NeXLCore.matches(KRatio(
             characteristic(n"Fe", ltransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             uv(0.6517, 0.02),
-            ), fe2std)
-        @test !matches(KRatio(
+            ), fe2)
+        @test !NeXLCore.matches(KRatio(
             characteristic(n"Fe", ltransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             uv(0.6517, 0.02),
-            ), fe1std)
-        @test !matches(KRatio(
+            ), fe1)
+        @test !NeXLCore.matches(KRatio(
             characteristic(n"Fe", kalpha),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             uv(0.45, 0.02),
-        ), ca1std)
+        ), ca1)
         stdized = standardize([ KRatio(
                 characteristic(n"Fe", kalpha),
                 Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
@@ -79,7 +81,7 @@ using Test
                 Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
                 mat"LiF",
                 uv(0.333, 0.02)) ],
-            [ fe1std, fe2std, ca1std, ca2std, sistd ])
+            [ fe1, fe2, ca1, ca2, si1 ])
         @test value(stdized[1]) ==0.45/0.6517
         @test isequal(stdized[1].standard, mat"Fe2O3")
         @test value(stdized[2])==0.32/0.7280
@@ -90,63 +92,60 @@ using Test
     @testset "Standardize KRatios" begin
         fe1 = KRatio(
             characteristic(n"Fe", kalpha),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             uv(0.6517, 0.02),
         ) 
         fe2 = KRatio(
             characteristic(n"Fe", ltransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             uv(0.3217, 0.02),
         )
         ca1 = KRatio(
             characteristic(n"Ca", kalpha),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Ca5(PO4)3F"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"CaF2",
             uv(0.7280, 0.02),
         )
         ca2 = KRatio(
             characteristic(n"Ca", kbeta),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Ca5(PO4)3F"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"CaF2",
             uv(0.7326, 0.02),
         )
         si1 = KRatio(
             characteristic(n"Si", ktransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"SiO2"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Si",
             uv(0.3801, 0.02),
         )
-        fe1std, fe2std = Standard(mat"Fe2O3", fe1), Standard(mat"Fe2O3", fe2)
-        ca1std, ca2std = Standard(mat"Ca5(PO4)3F", ca1), Standard(mat"Ca5(PO4)3F", ca2)
-        sistd = Standard(mat"SiO2", si1)
-        @test matches(KRatios(
+        @test NeXLCore.matches(KRatios(
             characteristic(n"Fe", ltransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             [ uv(0.6517, 0.02), uv(0.6517, 0.02) ]
-            ), fe2std)
-        @test !matches(KRatios(
+            ), fe2)
+        @test !NeXLCore.matches(KRatios(
             characteristic(n"Fe", ltransitions),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             [ uv(0.6517, 0.02), uv(0.6517, 0.02) ]
-            ), fe1std)
-        @test !matches(KRatios(
+            ), fe1)
+        @test !NeXLCore.matches(KRatios(
             characteristic(n"Fe", kalpha),
-            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
+            Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0), :Composition => mat"Fe2O3"),
             Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
             mat"Fe",
             [ uv(0.45, 0.02), uv(0.45, 0.02) ]
-        ), ca1std)
+        ), ca1)
         stdized = standardize([ KRatios(
                 characteristic(n"Fe", kalpha),
                 Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
@@ -165,7 +164,7 @@ using Test
                 Dict(:BeamEnergy => 20.0e3, :TakeOffAngle => deg2rad(40.0)),
                 mat"LiF",
                 [ uv(0.333, 0.02), uv(0.333, 0.02) ]) ],
-            [ fe1std, fe2std, ca1std, ca2std, sistd ])
+            [ fe1, fe2, ca1, ca2, si1 ])
         @test value(stdized[1].kratios[1]) == 0.45/0.6517
         @test isequal(stdized[1].standard, mat"Fe2O3")
         @test value(stdized[2].kratios[1])==0.32/0.7280

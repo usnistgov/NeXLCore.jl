7db635078232f928ef4b2bd6c419bb53a6e0347b
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2022-08-03 10:50:09 -0400

    Few more refinements

--- src/dtsa_mac.jl
+++ src/dtsa_mac.jl
@@ -29,10 +29,10 @@ function mac(elm::Element, ev::Float64, ::Type{DTSA})::Float64
             nm = evalpoly(zz, (3.34745, 0.02652873, -0.01273815))
         else
             cc = evalpoly(zz, 1.0E-5 .* (525.3, 133.257, -7.5937, 0.169357, -0.0013975))
-            az = evalpoly(zz, (0.0, 47.0, 6.52, -0.152624))
+            az = zz * evalpoly(zz, (47.0, 6.52, -0.152624))
             nm = evalpoly(zz, (3.112, -0.0121))
             if (ev > ee[1]) && (zz >= 50)
-                az = evalpoly(zz, (0.0, 47.0, 3.52, -0.015))
+                az = zz * evalpoly(zz, (47.0, 3.52, -0.015))
             end
             if (ev > ee[1]) && (zz >= 57)
                 cc = evalpoly(zz, 1.0e-6 .* (200.0, 100.0, -1.0))
@@ -41,7 +41,7 @@ function mac(elm::Element, ev::Float64, ::Type{DTSA})::Float64
     elseif ev > ee[4]
         # ev is below K-edge & above L3-edge
         cc = (c = evalpoly(zz, (-0.0924e-3, 0.141478e-3, -0.00524999e-3, 9.85296E-8, -9.07306E-10, 3.19245E-12)))
-        az = evalpoly(zz, (0.0, 17.8096, 0.067429, 0.01253775, -0.000116286))
+        az = zz * evalpoly(zz, (17.8096, 0.067429, 0.01253775, -0.000116286))
         nm = evalpoly(zz, (2.7575, 0.001889, -4.982E-5))
         if (ev < ee[2]) && (ev > ee[3])
             cc = c * 0.858
@@ -58,14 +58,14 @@ function mac(elm::Element, ev::Float64, ::Type{DTSA})::Float64
             evalpoly(zz, (30039, -1736.63566, 40.424792, -0.40585911, 0.001497763))
         end
         cc = 1.0e-7 * c
-        az = evalpoly(zz, (0.0, 10.2575657, -0.822863477, 0.0263199611, -0.00018641019))
-        if zz < 61
-            bias = evalpoly(zz, (0.0, 5.654, -0.536839169, 0.018972278, -0.0001683474))
+        az = zz * evalpoly(zz, (10.2575657, -0.822863477, 0.0263199611, -0.00018641019))
+        bias = if zz < 61
+            zz * evalpoly(zz, (5.654, -0.536839169, 0.018972278, -0.0001683474))
         else
-            bias = evalpoly(zz, (0.0, -1232.4022, 51.114164, -0.699473097, 0.0031779619))
+            zz * evalpoly(zz, (-1232.4022, 51.114164, -0.699473097, 0.0031779619))
         end
     elseif ev >= ee[9]
-        az = evalpoly(zz, (0.0, 4.62, -0.04))
+        az = zz * evalpoly(zz, (4.62, -0.04))
         c = evalpoly(zz, 1.0e-8 .* (7770.8, -783.544, 22.09365, -0.129086)) * # 
             evalpoly(zz, (1.406, 0.0162, -0.0006561, 4.865e-6))
         cc = c * evalpoly(zz, (0.584, 0.01955, -0.0001285))
@@ -82,7 +82,7 @@ function mac(elm::Element, ev::Float64, ::Type{DTSA})::Float64
         end
     elseif ev < ee[9]
         cc = evalpoly(zz, 1.08E-7 .* (43156.0, -1465.3, 17.07073, -0.0669827))
-        az = evalpoly(zz, (0.0, 19.64, -0.61239, 0.00539309))
+        az = zz * evalpoly(zz, (19.64, -0.61239, 0.00539309))
         bias = evalpoly(zz, (-113.0, 4.5))
         nm = evalpoly(zz, (0.3736, 0.02401))
     end

0dec29c2f3421a3ebf4b988be4729d7ca00113ea
Nicholas W. M. Ritchie <nicholas.ritchie@nist.gov>
2022-08-03 12:48:27 -0400

    Implemented more electron related functions.

--- src/electron.jl
+++ src/electron.jl
@@ -4,21 +4,53 @@
 const mₑ = ustrip((ElectronMass * SpeedOfLightInVacuum^2) |> u"eV")
 
 """
+Relativistic γ for v in cm/s
+"""
+γₑ(v::Quantity) = 1.0 / sqrt(1.0 - (v/SpeedOfLightInVacuum)^2)
+γₑ(v::AbstractFloat) = γₑ(v*u"cm/s")
+
+"""
+Electron kinetic energy in eV for v in cm/s.
+"""
+Ekₑ(v::Quantity) = (γₑ(v) - 1.0) * ElectronMass * SpeedOfLightInVacuum^2 |> u"eV"
+Ekₑ(v::AbstractFloat) = ustrip( ((γₑ(v) - 1.0) * ElectronMass * SpeedOfLightInVacuum^2) |> u"eV")
+
+"""
+Electron velocity in cm/s for the specified kinetic energy in eV.
+"""
+function vₑ(Ek::Quantity)
+    γ = 1.0 + Ek/(ElectronMass*SpeedOfLightInVacuum^2)
+    sqrt(1.0 - (1.0 / γ)^2) * SpeedOfLightInVacuum |> u"m/s"
+end
+function vₑ(Ek::AbstractFloat)
+    γ = 1.0 + Ek/mₑ
+    ustrip(sqrt(1.0 - (1.0 / γ)^2) * SpeedOfLightInVacuum |> u"cm/s")
+end
+
+
+"""
     λₑ(E::Float64)
 
-Wavelength of an electron in cm. (non-relativistic)
+Wavelength of an electron in cm.
 
   * E in eV
 """
-function λₑ(E::Float64)
-    return 1.0e2 * ustrip(PlanckConstant / sqrt(2.0 * ElectronMass * ElementaryCharge * E * u"eV")) # cm
+function λₑ(Ek::Quantity)
+    v = vₑ(Ek)
+    PlanckConstant*sqrt(1.0-(v/SpeedOfLightInVacuum)^2)/(ElectronMass*v) |> u"m"
 end
-
+λₑ(E::Float64) = ustrip(λₑ(E*u"eV") |> u"cm")
 """
     kₑ(E::Float64)
 
 Electron wavenumber (inverse wavelength) in rad⋅cm⁻¹.
 """
-kₑ(E::Float64) = 2π / λₑ(E)
-# = 0.01*sqrt(2.0*9.109e-31*1.602e-19*E)/1.05457e-34
+kₑ(E::Quantity) = 2π / λₑ(E)
+kₑ(E::Float64) = ustrip(kₑ(E*u"eV") |> "cm^-1")
 
+
+"""
+Electrons per second per nA of current.
+"""
+electrons_per_second(ic::Quantity) = ic/ElementaryCharge |> u"s^-1"
+electrons_per_second(ic::AbstractFloat) = electrons_per_second(ic*u"nA")
--- test/electron.jl
+++ test/electron.jl
@@ -6,10 +6,11 @@ using NeXLCore
 
     @test isapprox(λₑ(1.0), 1.227e-7, atol = 0.001e-7)
     @test isapprox(λₑ(100.0), 1.227e-8, atol = 0.001e-8)
-    @test isapprox(λₑ(1.0e4), 1.227e-9, atol = 0.001e-9)
+    @test isapprox(λₑ(1.0e4), 1.22e-9, atol = 0.001e-9)
     @test isapprox(λₑ(5.0e3), 0.0173 * 1.0e-7, atol = 0.0001 * 1.0e-7)
-    @test isapprox(λₑ(10.0e3), 0.0122 * 1.0e-7, atol = 0.0001 * 1.0e-7)
-    @test isapprox(λₑ(200.0e3), 0.0027 * 1.0e-7, atol = 0.0001 * 1.0e-7)
+    @test isapprox(λₑ(10.0e3), 0.01227 * 1.0e-7, atol = 0.0001 * 1.0e-7)
+    @test isapprox(λₑ(200.0e3), 0.0025 * 1.0e-7, atol = 0.0001 * 1.0e-7)
+    @test isapprox(λₑ(51.0u"GeV"), 2.4e-17u"m", atol=0.1e-17u"m")
 
     @test isapprox(
         kₑ(1.0),
@@ -31,11 +32,18 @@ using NeXLCore
         0.01 * sqrt(2.0 * 9.109e-31 * 1.602e-19 * 200.0) / 1.05457e-34,
         rtol = 1.0e-3,
     )
-    @test isapprox(
-        kₑ(2000.0),
-        0.01 * sqrt(2.0 * 9.109e-31 * 1.602e-19 * 2000.0) / 1.05457e-34,
-        rtol = 1.0e-3,
-    )
-
+    @test isapprox(kₑ(2000.0), 2.2933e9, rtol = 1.0e-3)
     @test isapprox(mₑ, 0.511e6, atol = 0.001e6)  # Must be in eV!!!!
+
+    @test isapprox(NeXLCore.electrons_per_second(1.0u"nA"), 6.24e9*u"s^-1", atol=0.01e9*u"s^-1")
+    @test isapprox(NeXLCore.electrons_per_second(1.0), 6.24e9, atol=0.01e9)
+
+    @test isapprox(NeXLCore.vₑ(0.511e6), 2.596e10, atol = 0.001e10)
+    @test isapprox(NeXLCore.vₑ(0.511e6u"eV"), 2.596e10u"cm/s", atol = 0.001e10u"cm/s")
+
+    @test isapprox(NeXLCore.vₑ(51e9), ustrip(SpeedOfLightInVacuum |> u"cm/s"), rtol = 0.001)
+    @test isapprox(NeXLCore.vₑ(51u"GeV"), SpeedOfLightInVacuum, rtol = 0.001)
+
+    @test isapprox(NeXLCore.γₑ(0.999*SpeedOfLightInVacuum), 22.36, rtol = 0.001)
+    @test isapprox(NeXLCore.γₑ(0.01*SpeedOfLightInVacuum), 1.0, rtol = 0.001)
 end
